4f6a710c590264ac4bf0dd85d836abf9c4cc252c
Katia Laws <knadia@live.com>
2024-11-06 13:14:18 +0000

    Add addClass jQuery method

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     $("h2").addClass("underline"); //adds underline to all h2 headings
-    4("ul").addClass("border"); //adds a border to all lists
+    $("ul").addClass("border"); //adds a border to all lists
 });
